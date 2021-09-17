ac7c9fb54c658a73cee4a6350a6185147e630c4f
MayaTanaka <ta53o.ma@gmail.com>
2021-09-17 07:48:57 +0000

    [Finish]2-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -33,7 +33,7 @@ def index
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+     params.require(:list).permit(:title, :body, :image)
   end
 end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '18f57ba9cc4b718c825f9b19d132355cbfcd0da8e4a00f5017fc6593c8312f37f809a2710098f6f31adb7d752ed783f23474b88a64f43ac29d51264a6bc8313c'
